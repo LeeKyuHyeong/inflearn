a7caf3e0c6c93cad466bf60f1d8a2e6b2142767a
이규형 <rbgud2380@naver.com>
2022-08-16 02:52:34 +0900

    Edit Appconfig.java2

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,13 +15,15 @@ public class AppConfig {
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-    //메모리멤버리파지토리 -> JDBC멤버리파지토리 변경시 얘만 변경하면 다 바뀜
+    //메모리멤버리파지토리 -> JDBC멤버리파지토리 변경시 얘만 변경하면 다 바뀜 dip
     private MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
+        //return new RateDiscountPolicy();
+        //기존의 클라이언트 코드에 변경없이 갈아끼우기 가능 ocp open close policy
     }
 
     public OrderService orderService() {
