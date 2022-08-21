16d0124f15aff4e746ce873e034cc3ad09b8a519
이규형 <rbgud2380@naver.com>
2022-08-22 01:20:37 +0900

    Add @Configuration

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
     //메모리멤버리파지토리 -> JDBC멤버리파지토리 변경시 얘만 변경하면 다 바뀜 dip
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
-        //return new RateDiscountPolicy();
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
         //기존의 클라이언트 코드에 변경없이 갈아끼우기 가능 ocp open close policy
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
