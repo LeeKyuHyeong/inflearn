463ddbf5eea2336e070618aa69c5c5e3f3899a35
이규형 <rbgud2380@naver.com>
2022-09-19 22:01:52 +0900

    autuwiredTest.java

--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -1,13 +1,19 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void contextLoads() {
+
+    }
 
 }
--- core/src/test/java/hello/core/autowired/AutowiredTest.java
+++ core/src/test/java/hello/core/autowired/AutowiredTest.java
@@ -1,2 +1,39 @@
-package hello.core.autowired;public class AutowiredTest {
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            //아무거나 집어넣은거다
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            //아무거나 집어넣은거다
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            //아무거나 집어넣은거다
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
 }
