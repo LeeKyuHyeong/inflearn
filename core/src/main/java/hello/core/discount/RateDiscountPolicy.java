7cc9c7b8b5ba760e0a8c3dd73b4d3c8fd30461f9
이규형 <rbgud2380@naver.com>
2022-09-24 15:30:27 +0900

    Edit with AllBeanTest

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,14 +1,12 @@
 package hello.core.discount;
 
-import hello.core.annotation.MainDiscountPolicy;
+import hello.core.annotation.mainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@MainDiscountPolicy
+@mainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
     
     private int rateDiscountAmount = 10; //10퍼센트
