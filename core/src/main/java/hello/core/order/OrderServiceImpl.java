208f06ee542fc23cd944a55975ec721d031a5064
이규형 <rbgud2380@naver.com>
2022-09-24 14:11:20 +0900

    @Primary use

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component()
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     //여기서도 Memory레파지토리랑 Fix할인정책 전혀 없다 DIP지키는중 의존관계주입 Dependency Injection Policy
     //역할 구분 잘 되있는 코드 (관심사 분리)
