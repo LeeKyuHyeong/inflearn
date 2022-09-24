1040de07ea0adbaa827af2deea45864cbbca615b
이규형 <rbgud2380@naver.com>
2022-09-25 07:49:35 +0900

    Add SingletonWithPrototypeTest.java

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,2 +1,53 @@
-package hello.core.scope;public class SingletonWithPrototypeTest1 {
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCnt();
+        assertThat(prototypeBean1.getCnt()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCnt();
+        assertThat(prototypeBean2.getCnt()).isEqualTo(1);
+
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int cnt = 0;
+
+        public void addCnt() {
+            cnt++;
+        }
+
+        public int getCnt() {
+            return cnt;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+
+    }
 }
