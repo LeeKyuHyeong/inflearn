ea1d5b7f484dee62b6a99de15fdfe0bc4176c74e
이규형 <rbgud2380@naver.com>
2022-09-27 00:02:25 +0900

    new Test

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,6 +3,7 @@ package hello.core.scope;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -35,13 +36,14 @@ public class SingletonWithPrototypeTest1 {
         assertThat(cnt1).isEqualTo(1);
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
-        int cnt2 = clientBean1.logic();
+        int cnt2 = clientBean2.logic();
         assertThat(cnt2).isEqualTo(2);
     }
 
     @Scope("singleton")
     static class ClientBean {
         private final PrototypeBean prototypeBean;  //생성시점에 주입
+
 
         @Autowired
         public ClientBean(PrototypeBean prototypeBean) {
